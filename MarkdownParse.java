b5a6f2e5e88a61389cb57743977608121f316ecd
canitry <56244521+canitry@users.noreply.github.com>
2022-04-14 15:25:06 -0700

    Fixed the emptyline at end causing infintel loop
    If any of the close bracket, open bracket, open parenthesis, or close parenthesis do not exist after where the currentIndex is, then the indexOf returns -1, causing the search for the rest of the open, close, parenthesis and brackets to loop back to the beginning

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,27 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        System.out.println(currentIndex);
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (openBracket == -1){
+                break;
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket == -1){
+                break;
+            }
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1){
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if (closeParen == -1){
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            // System.out.println("updated" + currentIndex);
+            // System.out.println("markdown" + markdown.length());
         }
 
         return toReturn;
