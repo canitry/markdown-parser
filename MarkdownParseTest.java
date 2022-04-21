e9a0399921c174f1b10cfc8705a52a08d14251a0
canitry <56244521+canitry@users.noreply.github.com>
2022-04-21 14:54:03 -0700

    added test of test-file.md
    created a successful test of test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+import java.io.IOException;
+//import java.util.ArrayList;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1+1);
+    }
+
+    @Test
+    public void testTestFile() throws IOException
+    {
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Paths.get("test-file.md"))));
     }
 }
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
