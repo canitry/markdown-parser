4de78a630142b5ed7e395cd9f18f15258d8490e9
canitry <56244521+canitry@users.noreply.github.com>
2022-04-21 15:14:54 -0700

    tests for 2-8
    tests have been written for the other testfiles

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -34,6 +34,48 @@ public class MarkdownParseTest {
     {
         assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("bracketswoparen.md"))));
     }
+
+    @Test
+    public void test2() throws IOException
+    {
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(Paths.get("test-file2.md"))));
+    }
+
+    @Test
+    public void test3() throws IOException
+    {
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("test-file3.md"))));
+    }
+
+    @Test
+    public void test4() throws IOException
+    {
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("test-file4.md"))));
+    }
+
+    @Test
+    public void test5() throws IOException
+    {
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("test-file5.md"))));
+    }
+
+    @Test
+    public void test6() throws IOException
+    {
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("test-file6.md"))));
+    }
+
+    @Test
+    public void test7() throws IOException
+    {
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Paths.get("test-file7.md"))));
+    }
+
+    @Test
+    public void test8() throws IOException
+    {
+        assertEquals(List.of("a link on the first line"), MarkdownParse.getLinks(Files.readString(Paths.get("test-file8.md"))));
+    }
 }
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
