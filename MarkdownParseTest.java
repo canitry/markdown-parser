a054210becccbd4f3aab4473993acc9b76b6538c
canitry <56244521+canitry@users.noreply.github.com>
2022-05-19 10:37:19 -0700

    added snippets to my own markdownparse
    trial 1, added according to what the preview counts as links. Will check on actual github repo what the md counts

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -88,6 +88,24 @@ public class MarkdownParseTest {
     {
         assertEquals(List.of("(((((9)))))", "hellotherewhatisyourname"), MarkdownParse.getLinks(Files.readString(Paths.get("newFail.md"))));
     }
+
+    @Test
+    public void testSnippet1() throws IOException
+    {
+        assertEquals(List.of("`google.com"), MarkdownParse.getLinks(Files.readString(Paths.get("snippet1.md"))));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException
+    {
+        assertEquals(List.of("a.com", "a.com(())", "example.com"), MarkdownParse.getLinks(Files.readString(Paths.get("snippet2.md"))));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException
+    {
+        assertEquals(List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedu", "https://cse.ucsd.edu/"), MarkdownParse.getLinks(Files.readString(Paths.get("snippet3.md"))));
+    }
 }
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java 0,25,26,38
 //java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
